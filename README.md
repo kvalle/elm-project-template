9478b7b7ad66ec07f8c63019df61d439c2255047
Kjetil Valle <kjetil.valle@gmail.com>
2017-12-28 14:42:37 +0100

    inital commit

---
+++ README.md
@@ -0,0 +1,3 @@
+A template to use for new Elm projects.
+
+Build with `npm run build`, start development server with `npm run dev`.
---
+++ src/Main.elm
@@ -0,0 +1,41 @@
+module Main exposing (main)
+
+import Html exposing (..)
+
+
+type Model
+    = Foo
+    | Bar
+
+
+type Msg
+    = NoOp
+
+
+init : ( Model, Cmd Msg )
+init =
+    ( Foo
+    , Cmd.none
+    )
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        NoOp ->
+            ( model, Cmd.none )
+
+
+view : Model -> Html Msg
+view model =
+    text "🌳 hello world"
+
+
+main : Program Never Model Msg
+main =
+    Html.program
+        { view = view
+        , init = init
+        , update = update
+        , subscriptions = always Sub.none
+        }
